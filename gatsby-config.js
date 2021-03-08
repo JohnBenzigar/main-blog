1901231373b11667725ac5f3a2e3c90b8e60bbdb
RoyallyCheese <67687772+RoyallyCheese@users.noreply.github.com>
2021-03-08 14:05:25 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Blog Boost Starter`,
+    title: `Skin Manic #1`,
     description: `A Gatsby Netlify CMS powered Blog Starter to boost your development.`,
     author: `@boostcode`,
   },
